ba105c6535ffd3fe21d27ad5ba31f20358d2ead6
Jeff Jian <jianyueting@gmail.com>
2019-11-15 10:21:45 +0800

    增加run.sh

--- 01-base/run.sh
+++ 01-base/run.sh
@@ -3,12 +3,7 @@
 # File Name: run.sh
 # Author: jeff
 # mail: jeff_jian@126.com
-# Created Time: Fri Nov 15 10:14:49 2019
+# Created Time: Fri Nov 15 10:18:00 2019
 #########################################################################
 
-base=/tmp
-data_dir=${base}/redis-data
-
-mkdir -p ${data_dir}
-
-docker run -d -p 6379:6379 -v ${data_dir}:/data debian-redis
+docker run --rm -it debian-base bash
