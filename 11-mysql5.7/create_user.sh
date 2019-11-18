3195a6e814e8d9ca124b717ff5ac1ed9a237c52a
Jeff Jian <jianyueting@gmail.com>
2019-11-18 16:28:52 +0800

    mysql配置文件追加空行，方便脚本里面追加配置 mysql创建用户增加是否有用户判断 添加mysql主从复制

--- 11-mysql5.7/create_user.sh
+++ 11-mysql5.7/create_user.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-"123456"}
-mysql -u root -e "grant all privileges on *.* to 'root'@'%' identified by '"$MYSQL_PASSWORD"' with grant option;flush privileges;"
+
+result=$(mysql -u root -e "select count(1) cnt from mysql.user where host='%' and user='root';"|grep -q 1 &>/dev/null;echo $?)
+[ ${result} -eq 0 ] || mysql -u root -e "grant all privileges on *.* to 'root'@'%' identified by '"$MYSQL_PASSWORD"' with grant option;flush privileges;"
